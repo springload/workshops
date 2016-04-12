842a0f3821665ff8408cd3215f6ecd2ce3b0bc32
Thibaud Colas <thibaudcolas@gmail.com>
2016-04-12 12:31:46 +1200

    Re-indent the code

--- writing-good-code/src/gilded_rose.js
+++ writing-good-code/src/gilded_rose.js
@@ -1,7 +1,7 @@
 function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
+    this.name = name;
+    this.sell_in = sell_in;
+    this.quality = quality;
 }
 
 var items = []
@@ -14,55 +14,56 @@ items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality(items) {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (items[i].quality > 0) {
+                if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                    items[i].quality = items[i].quality - 1
+                }
             }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1
+                if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (items[i].sell_in < 11) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
+                        }
+                    }
+                    if (items[i].sell_in < 6) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
+                        }
+                    }
+                }
+            }
         }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            items[i].sell_in = items[i].sell_in - 1;
         }
-      }
+
+        if (items[i].sell_in < 0) {
+            if (items[i].name != 'Aged Brie') {
+                if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (items[i].quality > 0) {
+                        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                            items[i].quality = items[i].quality - 1
+                        }
+                    }
+                } else {
+                    items[i].quality = items[i].quality - items[i].quality
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
+                }
+            }
+        }
     }
-  }
 }
 
 module.exports = {
-  Item: Item,
-  items: items,
-  update_quality: update_quality,
+    Item: Item,
+    items: items,
+    update_quality: update_quality,
 };
